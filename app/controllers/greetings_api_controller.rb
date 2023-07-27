ed003c3be100e8f7db8002006df98e38dcc1d8b0
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-07-27 12:58:39 +0200

    Add linters

--- app/controllers/greetings_api_controller.rb
+++ app/controllers/greetings_api_controller.rb
@@ -1,14 +1,14 @@
 class GreetingsApiController < ApplicationController
   GREETINGS = [
-    "Hello, how are you?",
-    "Hi there! Welcome!",
-    "Greetings and salutations!",
-    "Hey, nice to see you!",
-    "Hola! ¿Cómo estás?"
+    'Hello, how are you?',
+    'Hi there! Welcome!',
+    'Greetings and salutations!',
+    'Hey, nice to see you!',
+    'Hola! ¿Cómo estás?'
   ].freeze
 
   def random_greeting
     greeting = GREETINGS.sample
-    render json: { greeting: greeting }
+    render json: { greeting: }
   end
 end
